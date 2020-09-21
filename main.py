861d4dd6713d22b88bf4d5f0db8f163490a9a3d1
黄炜恒3118005365 <732119047@qq.com>
2020-09-21 17:29:19 +0800

    第二版代码 采用MinHash算法

--- main.py
+++ main.py
@@ -1,55 +1,65 @@
+# -*- coding: utf-8 -*-
+
+# 正则包
+import re
+# 自然语言处理包
 import jieba
-from gensim import corpora,models,similarities
-from collections import defaultdict   # 用于创建一个空的字典，在后续统计词频可清理频率少的词语
+import jieba.analyse
+# html 包
+import html
+# 数据集处理包
+from datasketch import MinHash
+import sys # 从命令行获取参数包
 
-# 1、读取文档
+class MinHashSimilarity(object):
+    """
+    MinHash
+    """
+    def __init__(self, content_x1, content_y2):
+        self.s1 = content_x1
+        self.s2 = content_y2
 
-doc1 = "/Users/rebecca/3118005365/test/orig.txt"
+    @staticmethod
+    def extract_keyword(content):  # 提取关键词
+        # 正则过滤 html 标签
+        re_exp = re.compile(r'(<style>.*?</style>)|(<[^>]+>)', re.S)
+        content = re_exp.sub(' ', content)
+        # html 转义符实体化
+        content = html.unescape(content)
+        # 切割
+        seg = [i for i in jieba.cut(content, cut_all=True) if i != '']
+        # 提取关键词
+        keywords = jieba.analyse.extract_tags("|".join(seg), topK=200, withWeight=False)
+        return keywords
 
-d1 = open(doc1,encoding='UTF-8').read()
+    def main(self):
+        # 去除停用词
+        # jieba.analyse.set_stop_words('./files/stopwords.txt')
 
-# 2、对要计算的文档进行分词
-data1 = jieba.cut(d1)
+        # MinHash计算
+        m1, m2 = MinHash(), MinHash()
+        # 提取关键词
+        s1 = self.extract_keyword(self.s1)
+        s2 = self.extract_keyword(self.s2)
 
-# 3、对分词完的数据进行整理为指定格式
-data11 = ""
-for i in data1:
-    data11 += i+" "
+        for data in s1:
+            m1.update(data.encode('utf8'))
+        for data in s2:
+            m2.update(data.encode('utf8'))
 
-documents = [data11]
+        return m1.jaccard(m2)
 
-texts = [[word for word in document.split()] for document in documents]
-print(texts)
-# 4、 计算词语的频率
-frequency = defaultdict(int)
-for text in texts:
-    for word in text:
-        frequency[word] += 1
-'''
-#5、对频率低的词语进行过滤（可选）
-texts=[[word for word in text if frequency[word]>10] for text in texts]
-'''
-# 6、通过语料库将文档的词语进行建立词典
-dictionary = corpora.Dictionary(texts)
-dictionary.save("/Users/rebecca/3118005365/test/dict.txt")    # 可以将生成的词典进行保存
-# 7、加载要对比的文档
-doc3 = "/Users/rebecca/3118005365/test/orig_0.8_add.txt"
-d3 = open(doc3, encoding='UTF-8').read()
-data3 = jieba.cut(d3)
-data31 = ""
-for i in data3:
-    data31 += i+" "
 
-# 8、将要对比的文档通过doc2bow转化为稀疏向量
-new_xs = dictionary.doc2bow(data31.split())
-# 9、对语料库进一步处理，得到新语料库
-corpus = [dictionary.doc2bow(text)for text in texts]
-# 10、将新语料库通过tf-idf model 进行处理，得到tfidf
-tfidf = models.TfidfModel(corpus)
-# 11、通过token2id得到特征数
-featurenum = len(dictionary.token2id.keys())
-# 12、稀疏矩阵相似度，从而建立索引
-index = similarities.SparseMatrixSimilarity(tfidf[corpus], num_features=featurenum)
-# 13、得到最终相似结果
-sim = index[tfidf[new_xs]]
-print(sim)
+# 测试
+if __name__ == '__main__':
+    article1_address = sys.argv[1] # 利用sys库，使程序可以从命令行获得原文地址参数
+    article2_address = sys.argv[2] # 利用sys库，使程序可以从命令行获得抄袭文本地址参数
+    answer_address = sys.argv[3] # 利用sys库，使程序可以从命令行获得输出答案地址参数
+    with open(article1_address, 'r') as x, open(article2_address, 'r') as y:
+        content_x = x.read()
+        content_y = y.read()
+        similarity = MinHashSimilarity(content_x, content_y)
+        similarity = similarity.main()
+        with open(answer_address, 'w') as f:
+            f.write('相似度: %.2f%%' % (similarity*100))
+
